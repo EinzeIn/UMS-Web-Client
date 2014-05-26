c4db49b043f1c6bb020348abe11422ac7e7bf099
EinzeIn <einzeln@live.com>
2014-05-27 06:40:48 +0900

    Add SOAP Request function

--- main.js
+++ main.js
@@ -21,3 +21,19 @@ chrome.app.runtime.onLaunched.addListener(function() {
     }
   });
 });
+(function soapReq(){
+  var dirReq = new XMLHttpRequest();
+  dirReq.open('POST', "192.168.1.8:1400/renderer/rendercontrol/control HTTP/1.1/", true);
+  var reqPacket = '<?xml version="1.0" encoding="utf-8"?>'+
+  '<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://scemas.xmlsoap.org/soap/envelope/">'+
+  '<s:Body>'+
+  '<u:BrowseResponse xmlns:u="urn:schemas-upnp-org:service:ContentDirectory:1">'+
+  '</u:BrowseResponse>'+
+  '</s:Body>'+
+  '</s:Envelope>';
+
+  dirReq.setRequestHeader("SOAPACTION", "urn:schemas-upnp-org:service:ContentDirectory:1#Browse");
+  dirReq.setRequestHeader("CONTENT-TYPE",  "text/xml;charset=UTF-8");
+
+  dirReq.send(reqPacket);
+});
